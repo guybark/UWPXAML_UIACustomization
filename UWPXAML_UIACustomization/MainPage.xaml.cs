693a69494ce31ab692fd57836f070d45921f61f9
TheDoctor <TheDoctor@Gallifrey.org>
2021-05-12 10:36:02 -0700

    Add project files.

---
+++ UWPXAML_UIACustomization/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPXAML_UIACustomization
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+
+
+    public class MyGraph : UserControl
+    {
+        // If it's necessary for the custom control to have its own
+        // custom AutomationPeer, then additional customization of the
+        // UIA representation can be made through the peer.
+        //protected override AutomationPeer OnCreateAutomationPeer()
+        //{
+        //    return new MyGraphAutomationPeer(this);
+        //}
+    }
+
+    //public class MyGraphAutomationPeer : FrameworkElementAutomationPeer
+    //{
+    //    public MyGraphAutomationPeer(MyGraph owner) :
+    //        base(owner)
+    //    {
+    //    }
+    //}
+
+}
